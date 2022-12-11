39ea06647aa0e871a0415580874b62b4ad2ecdcc
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-12-11 11:45:59 +0530

    update

--- movieapp/src/Navbar.js
+++ movieapp/src/Navbar.js
@@ -5,7 +5,8 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-const navbar = () => {
+
+const Navbar1 = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
@@ -48,4 +49,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar1
