9bdb0dceb0a80dd1f0cd684644973c411c65fb8d
Soham manjrekar <mrsohammanjrekar@gmail.com>
2023-01-10 23:28:32 +0530

    Update App.js

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import SingleMovie from "./SingleMovie";
+import SingleMovie from "./singlemovie";
 import "./App.css";
 
 const App = () => {
